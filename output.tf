8c1df44f01cf84930508ada9af70fc6c155409ba
msaivenkatasiva <sivasl0597@gmail.com>
2025-08-15 15:56:51 +0530

    terraform-vpc-source-module

--- output.tf
+++ output.tf
@@ -10,6 +10,22 @@ output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
 
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
+
+output "database_subnet_group_id" {
+  value = aws_db_subnet_group.default.id
+}
+
+output "database_subnet_group_name" {
+  value = aws_db_subnet_group.default.name
+}
+
 output "igw_id" {
-    value = aws_internet_gateway.gw.id
+  value = aws_internet_gateway.gw.id
 }
